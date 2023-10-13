329cbcb881dff99b2cbeddbf9526dca7c2d5f759
Pedro Rivera <subparmedic@outlook.com>
2023-10-13 17:47:40 -0400

    Updated dependencies, gpt model
    Updated to openai v4, adjusted code accordingly, and added code safety for discord markup.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const Discord = require('discord.js'); 
-const { Configuration, OpenAIApi } = require("openai");
+const OpenAI = require('openai');
 const express = require('express');
 
 const app = express();
 
 require('dotenv').config();
 
-const configuration = new Configuration({
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+});
 
 const client = new Discord.Client({
     intents: [
@@ -21,7 +20,6 @@ const client = new Discord.Client({
         Discord.IntentsBitField.Flags.DirectMessageTyping
     ]
 });
-
 //required for hosting on HEROKU, without specifying the port, it will crash shortly after deployment
 
 const PORT = process.env.PORT; 
@@ -41,29 +39,38 @@ client.on('ready', () => {
 //It stores the question in a const called stringPrompt, and includes it in the prompt.
 
 client.on('messageCreate', async msg => {
-    const stringPrompt = msg.content;
-    const triggerCommand = '!question';
-    
-    if (stringPrompt.includes(triggerCommand)) {
+  const stringPrompt = msg.content;
+  const triggerCommand = '!question';
+  const codeSafety = ' (Surround any code blocks with three ` characters so it will display correctly. Ex. ```Code```';
+  
+  if (stringPrompt.includes(triggerCommand)) {
 
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: stringPrompt,
-            temperature: 0.7,
-            max_tokens: 256,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
+      try {
+          const response = await openai.completions.create({
+              model: "gpt-4",
+              prompt: stringPrompt + codeSafety,
+              temperature: 0.7,
+              max_tokens: 800,
+              top_p: 1,
+              frequency_penalty: 0,
+              presence_penalty: 0,
           });
 
-          const generatedResponse = response.data.choices[0].text.trim();
+          const generatedResponse = response.choices[0].text.trim();
+          msg.reply(`Hello ${msg.author.username}, let me answer that for you: \n${generatedResponse}`);
 
-        msg.reply(`Hello ${msg.author.username}, let me answer that for you: \n${generatedResponse}`);
-    }
+      } catch (error) {
+          if (error instanceof OpenAI.APIError) { //per openai 4.0.0+
+              console.error(error.status);  // e.g., 401
+              console.error(error.message); // e.g., The authentication token you passed was invalid...
+              console.error(error.code);    // e.g., 'invalid_api_key'
+              console.error(error.type);    // e.g., 'invalid_request_error'
+          } else {
+              console.log(error);
+          }
+      }
+  }
 });
-
-
-
 
 client.login(process.env.CLIENT_TOKEN); 
 
